8d4289617638518ad9e65c14c76bb3e42a884e51
Deepak <206972401+thedeepak12@users.noreply.github.com>
2025-08-10 14:02:37 +0530

    Add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1 className="text-4xl font-bold text-center bg-white p-5 border-b-2 dark:border-gray-200">
+        Memory Card
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
