05ded7a036eab28eb160ab388032a73663c19669
Deepak <206972401+thedeepak12@users.noreply.github.com>
2025-08-12 13:14:21 +0530

    Update scoreboard

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Scoreboard from './Scoreboard';
 
-function Header() {
+function Header({ currentScore, bestScore }) {
   return (
-    <header className="flex justify-between border-b-2 border-gray-200">
-      <h1 className="text-4xl font-bold text-center p-5">Memory Card</h1>
-      <Scoreboard />
+    <header className="flex min-w-screen justify-between border-b-2 border-gray-200">
+      <h1 className="text-4xl font-bold text-center p-6 -mt-5">Memory Card</h1>
+      <Scoreboard currentScore={currentScore} bestScore={bestScore} />
     </header>
   );
 }
--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function Scoreboard() {
+function Scoreboard({ currentScore, bestScore }) {
   return (
     <div className="flex justify-center items-center">
-      <div className="flex justify-between items-center gap-4 bg-gray-200 p-5 mr-5 w-[300px] h-[60px] rounded-lg">
-        <p className="text-xl">Score: 0</p>
-        <p className="text-xl">Best Score: 0</p>
+      <div className="flex justify-between items-center gap-4 bg-gray-200 p-5 -mt-3 mr-5 w-[300px] h-[60px] rounded-lg">
+        <p className="text-xl">Score: {currentScore}</p>
+        <p className="text-xl">Best Score: {bestScore}</p>
       </div>
     </div>
   );
